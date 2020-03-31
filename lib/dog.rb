0245787f53d602ff7cc645f62efd5df446f6070e
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-03-31 16:12:16 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+  attr_accessor :name, :breed
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  attr_accessor :name, :job
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+end
